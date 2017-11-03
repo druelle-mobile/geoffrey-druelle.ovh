3f8a7b93a51d63d8ecc83c59275e72f602b3ef87
Geoffrey Druelle - Mobile <druelle.mobile@gmail.com>
2017-11-03 14:48:38 +0100

    Update experiences_associatives.php
    Correction line 32.

--- experiences_associatives.php
+++ experiences_associatives.php
@@ -29,7 +29,7 @@
             </div>
             <div class="demo-card-square mdl-card mdl-shadow--2dp mdl-cell--6-col mdl-cell--12-col-tablet">
               <div class="mdl-card__title mdl-card--expand">
-                <h2 class="mdl-card__title-text">Bureau des Etudiants en Sciences et Technologiques</h2>
+                <h2 class="mdl-card__title-text">Bureau des Etudiants en Sciences et Techniques</h2>
               </div>
               <div class="mdl-card__supporting-text">
                <ul>
